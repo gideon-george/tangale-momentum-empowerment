dae8cad7b3bc7d52873764ad75a084bdbf51dc9b
Gideon George <georgegideon@gmail.com>
2023-01-15 21:29:00 +0100

    fixing router

---
+++ src/pages/Faq.vue
@@ -0,0 +1,38 @@
+<template>
+    <div className="flex flex-col min-h-screen overflow-hidden">
+  
+      <!-- Site header -->
+      <Header />
+  
+      <!-- Page content -->
+      <main class="grow">
+  
+        <Hero />
+        
+        <Faqs />
+        <Cta />
+  
+      </main>
+      
+      <!-- Site footer -->
+      <Footer />    
+  
+    </div>
+  </template>
+  
+  <script>
+import Header from '../partials/Header.vue'
+import Hero from '../partials/Hero.vue'
+import Cta from '../partials/Cta.vue'
+import Footer from '../partials/Footer.vue'
+
+export default {
+  name: 'Faq',
+components: {
+  Header,
+  Hero,
+  Cta,
+  Footer
+}
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import Proposal from './pages/Proposal.vue'
+import AboutUs from './pages/AboutUs.vue'
+import ContactUs from './pages/ContactUs.vue'
+import Faq from './pages/Faq.vue'
 
 
 const routerHistory = createWebHistory()
@@ -20,6 +23,18 @@ const router = createRouter({
     {
       path: '/',
       component: Home
+    },
+    {
+      path: '/about-us',
+      component: AboutUs
+    },
+       {
+      path: '/faq',
+      component: Faq
+    },
+       {
+      path: '/contact-us',
+      component: ContactUs
     },
     {
       path: '/proposal-call',
